b57b6d37ba8b934a0ee0643d83a06939595663cd
Fabricio Jr <fabriciojuniorsilva@gmail.com>
2019-05-18 19:34:20 -0300

    Adicionado API para o jogo

---
+++ api/application/controllers/Login.php
@@ -0,0 +1,19 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Login extends CI_Controller {
+
+	public function index()
+	{
+		
+    }
+    
+    public function login(){
+
+        $this->load->model('user_model');
+
+
+
+    }
+
+}
---
+++ api/application/models/User_model.php
@@ -0,0 +1,16 @@
+<?php
+
+class User_model extends CI_Model {
+
+    private $id;
+    private $login;
+    private $password;
+    private $nome;
+    private $email;
+
+    private function doLogin($user, $password){
+
+    }
+
+
+}
