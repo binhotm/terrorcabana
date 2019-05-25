467d0441a48d1b3c6675e8e39024d30a302aa9f5
Fabricio Jr <fabriciojuniorsilva@gmail.com>
2019-05-24 21:02:27 -0300

    Commita da api

--- api/application/controllers/Login.php
+++ api/application/controllers/Login.php
@@ -2,17 +2,11 @@
 defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Login extends CI_Controller {
-
-	public function index()
-	{
-		
-    }
     
-    public function login(){
+    public function do_login(){
 
         $this->load->model('user_model');
-
-
+        
 
     }
 
