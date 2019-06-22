44061987702b41e66937e0815dc39b9b03dcef70
Fabricio Jr <fabriciojuniorsilva@gmail.com>
2019-06-22 18:56:22 -0300

    Atualizacoes do JOGO, correcao de BUGs etc

--- api/application/controllers/Login.php
+++ api/application/controllers/Login.php
@@ -14,8 +14,7 @@ class Login extends CI_Controller {
     
     public function logon($user= "", $password=""){
              
-        // Deletado por enquanto nao tempos suporte a JSON $this->load->library('Api');
-        
+        // Deletado por enquanto nao tempos suporte a JSON $this->load->library('Api');   
 
         $this->load->model('player_model');
 
@@ -33,6 +32,65 @@ class Login extends CI_Controller {
         echo $ret;
         
         //$this->api->expose($arr);
+    }
+
+
+    public function createAccount($name ="", $login="", $email="", $password="", $password2 = ""){
+        
+        $this->load->model('player_model');
+
+        if($login == ""){
+            echo "ERRO: Login em branco";
+            die();
+        }
+
+      
+        if(strlen($login) <= 3){
+            echo "ERRO: Login pequeno de mais.";
+            die();
+        }
+
+        if($password == ""){
+            echo "ERRO: Password em branco";
+            die();
+        }
+
+        if(strlen($password) <= 3){
+            echo "ERRO: Password pequeno de mais.";
+            die();
+        }
+
+
+        if(!$this->player_model->checkLogin($login)){
+            echo "ERRO: Login ja existe!";
+            die();
+        }
+        
+        if($login == "" && $password == ""){
+           echo "ERRO: Usuario ou Senha em Branco!";
+           die();
+        }
+
+        if(!$password == $password2){
+            echo "ERRO: Senhas não conferem.";
+            die();
+        } 
+
+
+        $data['name'] = $name;
+        $data['login'] = $login;
+        $data['email'] = $email;
+        $data['password'] = $password;
+        
+        $ret = $this->player_model->createAccount($data);
+
+        if($ret != 0 || $ret != null){
+            echo $ret;
+        }else{
+            echo "Erro ao cadastrar usuario! API ERROR 400.";
+        }
+
+        
     }
 
 }
